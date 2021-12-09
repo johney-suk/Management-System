082a6ca4466e5ad250629abc011e4049147fe580
sukhyunil <sukhyunil@naver.com>
2021-12-09 18:09:39 +0900

    changed customer. js

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -4,11 +4,32 @@ class Customer extends React.Component {
     render() {
         return(
         <div>
-            <h2>{this.props.name}</h2>
-            <p>{this.props.birthday}</p>
-            <p>{this.props.gender}</p>
-            <p>{this.props.job}</p>  
+                <CustomerProfile id={this.props.id} image= {this.props.image} name={this.props.name}/>
+                <CustomerInfo birthday={this.props.birthday} gender = {this.props.gender} job={this.props.job}/>
         </div>
+        )
+    }
+}
+
+class CustomerProfile extends React.Component {
+    render(){
+        return(
+            <div>
+                <img src={this.props.img} alt = "profile"/>
+                <h2> {this.props.name}({this.props.id})</h2>
+            </div>
+        )
+    }
+}
+
+class CustomerInfo extends React.Component{
+    render() {
+        return(
+            <div>
+                <p>{this.props.birthday}</p>
+                <p>{this.props.gender}</p>
+                <p>{this.props.job}</p>
+            </div>
         )
     }
 }
